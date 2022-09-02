9c4fd24275ef70d9c1ac15ba245848b64efa1249
SANDRA SHHOYOS <shadanahoyos@gmail.com>
2022-09-02 01:23:08 -0300

    primer commit

--- FRONT-END/src/app/app.module.ts
+++ FRONT-END/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    AcercaComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
